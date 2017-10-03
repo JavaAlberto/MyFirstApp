619e9aefde4399620c41a05d079543daf0f9522b
Albrecht Pfleiderer <JavaAlberto@freenet.de>
2017-10-03 17:29:19 +0200

    Erster Commit

--- app/src/main/java/berufsschulefreising/de/myfirstapp/MainActivity.java
+++ app/src/main/java/berufsschulefreising/de/myfirstapp/MainActivity.java
@@ -2,7 +2,6 @@ package berufsschulefreising.de.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
